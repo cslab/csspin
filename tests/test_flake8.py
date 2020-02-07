e135ac00101749f29fd3acfaed06a4cc3104243e
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2020-02-07 20:11:03 +0100

    make test more robust in case flake8 has something to complain

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -4,6 +4,6 @@ from wheel import pep425tags
 
 def test_flake8():
     runner = CliRunner()
-    result = runner.invoke(cli, ["--debug", "flake8"])
+    result = runner.invoke(cli, ["--debug", "flake8", "--exit-zero", "./tests"])
     assert result.exit_code == 0
     assert result.output.startswith("spin")
