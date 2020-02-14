5589d90d7fe094d0ebc5470643a111760db8215c
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2020-02-14 18:48:15 +0100

    add first test case for spin.cruise

---
+++ tests/test_cruise.py
@@ -0,0 +1,12 @@
+from spin import cli, cruise
+
+
+def test_cruise():
+    cfg = cli.load_spinfile("spinfile.yaml")
+
+    def match(*selectors):
+        return [name for name, _ in cruise.match_cruises(cfg, *selectors)]
+    
+    assert "cp27-win" in match("cp27-win")
+    assert "host" in match("@all")
+    assert "host" not in match("@docker")
