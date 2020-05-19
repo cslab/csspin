e3e95f8a721636a24a8ab7d0742468a9a37849a2
Vladimir Berditschewski <vbe@contact.de>
2020-05-19 08:20:42 +0200

    Buildout plugin added

---
+++ src/spin/builtin/buildout.py
@@ -0,0 +1,31 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 2020 CONTACT Software GmbH
+# All rights reserved.
+# http://www.contact.de/
+
+import os
+import shutil
+
+from spin import config, option, sh, task
+
+
+defaults = config(
+    opts=["-q", "-N"],
+    requires=[".virtualenv"],
+    packages=["zc.buildout"],
+)
+
+
+@task()
+def buildout(cfg,
+             instance: option("--instance", "instance"),
+             rebuild: option("--rebuild", is_flag=True),
+             args):
+    """Run the 'buildout' command."""
+    if not instance:
+        instance = cfg.buildout.instance
+    if rebuild and os.path.isdir(instance):
+        print("Deleting instance '%s'..." % instance)
+        shutil.rmtree(instance)
+    sh("buildout", *cfg.buildout.opts, "install", instance)
