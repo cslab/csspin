3b9a2a4cf5993a8f7b17ac1b345b6982336e46fb
Timo Stüber <tst@contact.de>
2020-08-07 11:36:39 +0200

    run cruise in interactive mode if no spinargs added

--- src/spin/cruise.py
+++ src/spin/cruise.py
@@ -59,13 +59,17 @@ def do_cruise(cfg, cruiseopt, interactive):
             spin_base_opts = False
         i += 1
     for name, definition in match_cruises(cfg, cruiseopt):
-        executor = definition.executor(name, definition, interactive)
         cmd = [getattr(definition, "cruise_spin", cfg.spin.cruise_spin)] + getattr(
             definition, "opts", []
         )
         for pname, pvalue in getattr(definition, "properties", {}).items():
             cmd += ["-p", f"{pname}={pvalue}"]
         cmd += spin_args
+        if len(spin_args) == 0:
+            # interactivly run a docker container having spin installed
+            interactive = True
+            cmd = ['bash', '-c', " ".join(cmd + [';', 'bash'])]
+        executor = definition.executor(name, definition, interactive)
         executor.run(cmd)
 
 
