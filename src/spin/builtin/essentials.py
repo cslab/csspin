be90a22cf98a6160bec01213d7dfc3ef615117d4
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2021-09-14 22:21:20 +0200

    add "propinfo" command to inspect schema

--- src/spin/builtin/essentials.py
+++ src/spin/builtin/essentials.py
@@ -13,3 +13,29 @@ def exec_shell(args):
     if not args:
         args = ("{platform.shell}",)
     sh(*args)
+
+
+def pretty_descriptor(prefix, name, descriptor):
+    typename = " ".join(getattr(descriptor, "type", ["any"]))
+    default = getattr(descriptor, "default", None)
+    decl = f"{prefix}{name}: {typename}"
+    if default:
+        decl += f" = '{default}'"
+    print(decl)
+    helptext = getattr(descriptor, "help", "").split("\n")
+    prefix = len(prefix) * " "
+    for line in helptext:
+        print(prefix + line)
+
+
+@task()
+def propinfo(cfg, args):
+    schema = cfg.schema
+    arg = "The Spinfile Schema"
+    for arg in args:
+        schema = schema.properties.get(arg)
+    helptext = getattr(schema, "help", "--")
+    pretty_descriptor("", arg, schema)
+    properties = getattr(schema, "properties", {})
+    for prop, desc in properties.items():
+        pretty_descriptor("* ", prop, desc)
