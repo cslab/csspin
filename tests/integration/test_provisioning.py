e1ad7c3323841c56ec270197ec25a080903370dd
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2021-09-21 21:20:15 +0200

    add (slow) integration tests for plugins
    this tests will provision temporary envs and check, whether the
    outcome is to be expected (this is slow)

---
+++ tests/integration/test_provisioning.py
@@ -0,0 +1,70 @@
+import pytest
+
+from spin import backtick, cli, sh, writetext
+
+
+@pytest.fixture(autouse=True)
+def cfg():
+    cli.load_config_tree(None)
+
+
+def do_test(tmpdir, what, cmd):
+    output = backtick(
+        f"spin -C tests/integration --env {tmpdir} -f {what} --provision {cmd}"
+    )
+    print(output)
+    return output
+
+
+@pytest.mark.slow
+def test_python(tmpdir):
+    assert do_test(
+        tmpdir,
+        "python.yaml",
+        "python --version",
+    ).endswith("Python 3.9.6\n")
+
+
+@pytest.mark.slow
+def test_cppcheck(tmpdir):
+    assert do_test(
+        tmpdir,
+        "cppcheck.yaml",
+        "run which cppcheck",
+    ).endswith("cppcheck\n")
+
+
+@pytest.mark.slow
+def test_cpplint(tmpdir):
+    assert do_test(
+        tmpdir,
+        "cpplint.yaml",
+        "run which cpplint",
+    ).endswith("cpplint\n")
+
+
+@pytest.mark.slow
+def test_flake(tmpdir):
+    assert do_test(
+        tmpdir,
+        "flake.yaml",
+        "run which flake8",
+    ).endswith("flake8\n")
+
+
+@pytest.mark.slow
+def test_buildout(tmpdir):
+    assert do_test(
+        tmpdir,
+        "buildout.yaml",
+        "run which buildout",
+    ).endswith("buildout\n")
+
+
+@pytest.mark.slow
+def test_build(tmpdir):
+    assert "all build tasks" in do_test(
+        tmpdir,
+        "build.yaml",
+        "build --help",
+    )
