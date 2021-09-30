33546d9cc7956ad56113fd72b9b1e8e5b3e3faf7
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2021-09-30 21:19:21 +0200

    remove outdated scripts

--- devrun.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Preparing an editable install of 'spin' ...
-pip uninstall -q -y virtualenv
-pip install -q "virtualenv<20.0"
-pip install -q -e .
-c:\Scoop\apps\python\current\Scripts\spin %*
