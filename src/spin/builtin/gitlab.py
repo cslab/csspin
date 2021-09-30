c8816de26c7f9138ba97a1b3cbfbe385e8ab4e4a
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2021-09-30 21:18:31 +0200

    add task 'gitlib-ci'
    this provisions gitlab-ci-local (https://github.com/firecow/gitlab-ci-local)

--- src/spin/builtin/gitlab.py
+++ src/spin/builtin/gitlab.py
@@ -7,19 +7,13 @@
 from spin import config, download, exists, normpath, sh, task
 
 defaults = config(
-    runner_url="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64",
-    cmd="{spin.cache}/{platform.tag}/gitlab-runner",
-    exec_options=[],
+    cmd="gitlab-ci-local",
+    requires=config(
+        npm=["gitlab-ci-local"],
+    ),
 )
 
 
-def ensure_gitlab_runner(cfg):
-    if not exists(cfg.gitlab.cmd):
-        download(cfg.gitlab.runner_url, cfg.gitlab.cmd)
-        sh("chmod", "+x", cfg.gitlab.cmd)
-
-
-@task(add_help_option=False)
+@task("gitlab-ci", add_help_option=False)
 def gitlab(cfg, args):
-    ensure_gitlab_runner(cfg)
     sh(cfg.gitlab.cmd, *args)
---
+++ src/spin/builtin/node.py
@@ -0,0 +1,37 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright (C) 2020 CONTACT Software GmbH
+# All rights reserved.
+# http://www.contact.de/
+
+from spin import config, die, get_requires, memoizer, sh
+
+defaults = config(
+    version=None,
+    requires=config(
+        spin=[".python"],
+        python=["nodeenv"],
+    ),
+    memo="{python.venv}/nodeversions.memo",
+)
+
+
+def configure(cfg):
+    if cfg.node.version is None:
+        die(
+            "Spin's Node.js plugin does not set a default version.\n"
+            "Please choose a version in spinfile.yaml by setting node.version"
+        )
+
+
+def provision(cfg):
+    with memoizer(cfg.node.memo) as m:
+        if not m.check(cfg.node.version):
+            sh("nodeenv", "-p", cfg.node.version)
+            m.add(cfg.node.version)
+        for plugin in cfg.topo_plugins:
+            plugin_module = cfg.loaded[plugin]
+            for req in get_requires(plugin_module.defaults, "npm"):
+                if not m.check(req):
+                    sh("npm", "install", "-g", req)
+                    m.add(req)
