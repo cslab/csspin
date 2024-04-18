c4b3e05ebd19ffaf31484f3226bc7058fdf57cc6
Benjamin Thomas Schwertfeger <benjaminthomas.schwertfeger@contact-software.com>
2024-04-18 05:17:49 +0000

    Extend API unit tests pt2

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,8 +15,9 @@ import os
 import pickle
 import subprocess
 import sys
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 from unittest import mock
+from unittest.mock import patch
 
 import click
 import pytest
@@ -26,7 +27,8 @@ import spin
 from spin.tree import ConfigTree
 
 if TYPE_CHECKING:
-    from typing import Callable
+
+    from typing import Any
 
     from pytest_mock.plugin import MockerFixture
 
@@ -50,7 +52,7 @@ def test_echo_quiet(cfg: ConfigTree, mocker: MockerFixture) -> None:
     assert not click.echo.called  # type: ignore[attr-defined]
 
 
-def test_info(cfg: ConfigTree, mocker: MockerFixture) -> None:
+def test_info(mocker: MockerFixture) -> None:
     """spin.info is info'ing if CONFIG.verbose is False"""
     mocker.patch("click.echo")
     marker = "ZIOhddu"
@@ -113,30 +115,30 @@ def test_cd(tmpdir: Path) -> None:
     assert cwd == os.getcwd()
 
 
-def test_exists() -> None:
+def test_exists(cfg: ConfigTree, tmpdir: Path) -> None:
     """spin.exists is able to validate the existence of directories"""
-    spin_cache = os.getenv("SPIN_CACHE")
-    assert os.path.isdir(spin_cache)
-    assert spin.exists("{SPIN_CACHE}")
-    assert spin.exists(spin_cache)
+    cfg["TMPDIR"] = tmpdir
+    assert os.path.isdir(tmpdir)
+    assert spin.exists("{TMPDIR}")
+    assert spin.exists(tmpdir)
     assert not spin.exists(r"\foo/bar\baz/biz\buz")
 
 
-def test_normpath() -> None:
+def test_normpath(cfg: ConfigTree) -> None:
     """
     spin.normpath is resolving environment variables to return the normalized
     path
     """
-    SPIN_CACHE = os.getenv("SPIN_CACHE")
-    assert spin.normpath("{SPIN_CACHE}") == os.path.normpath(SPIN_CACHE)
+    cfg["FOO"] = "foo"
+    assert spin.normpath("{FOO}") == os.path.normpath("foo")
 
 
-def test_abspath() -> None:
+def test_abspath(cfg: ConfigTree) -> None:
     """
     spin.abspath is resolving environment variables to return the absolute path
     """
-    SPIN_CACHE = os.getenv("SPIN_CACHE")
-    assert spin.abspath("{SPIN_CACHE}") == os.path.abspath(SPIN_CACHE)
+    cfg["FOO"] = "foo"
+    assert spin.abspath("{FOO}") == os.path.abspath("foo")
 
 
 def test_mkdir(tmpdir: Path) -> None:
@@ -179,7 +181,7 @@ def test_command() -> None:
     assert cmd._cmd == ["pip", "list", "--help"]
 
 
-def test_sh(cfg: ConfigTree, mocker: MockerFixture) -> None:
+def test_sh(mocker: MockerFixture) -> None:
     """
     spin.sh will raise the expected errors on faulty input as well as execute
     valid commands by calling subprocess.run with the correct arguments
@@ -400,24 +402,10 @@ def test_namespace_context_manager() -> None:
     assert not spin.NSSTACK
 
 
-def test_build_target(cfg: ConfigTree, mocker: MockerFixture) -> None:
-    """
-    spin.build_target extends the ConfigTree and is called using the expected
-    arguments
-    """
-    mocker.patch("subprocess.run")
-    cfg["build-rules"] = spin.config(
-        phony=spin.config(sources="notphony"), notphony=spin.config(script=["1", "2"])
-    )
-    # According to the rule above, building "phony" would require
-    # "notphony" to exist, which would be produced by calling 1 and 2.
-    spin.build_target(cfg, "phony", True)
-    assert [c.args[0][0] for c in subprocess.run.call_args_list] == ["1", "2"]  # type: ignore[attr-defined]
-
-
+@patch.dict(os.environ, {"FOO": "foo"})
 def test_interpolate1_env() -> None:
     """spin.interpolate1 interpolates environment variables as expected"""
-    assert spin.interpolate1("'{SPIN_CACHE}'") == f"'{os.environ['SPIN_CACHE']}'"
+    assert spin.interpolate1("'{FOO}'") == f"'{os.environ['FOO']}'"
 
 
 def test_interpolate1_values() -> None:
@@ -450,11 +438,12 @@ def test_interpolate1_onestep_recursion(cfg: ConfigTree) -> None:
     assert spin.interpolate1("{bad}") == "{bad}"
 
 
-def test_interpolate1_path() -> None:
+def test_interpolate1_path(cfg) -> None:
     """spin.interpolate1 is resolving a Path that must be interpolated"""
-    result = spin.interpolate1(Path("{SPIN_CACHE}"))
+    cfg["FOO"] = Path("foo")
+    result = spin.interpolate1(Path("{FOO}"))
     assert isinstance(result, Path)
-    assert result == os.getenv("SPIN_CACHE")
+    assert result == Path("foo")
 
 
 def test_interpolate_n() -> None:
@@ -518,7 +507,6 @@ def test_getmtime(tmpdir: Path) -> None:
 @mock.patch.dict(os.environ, {"TEST_CUSTOM_FILE": "text.txt"})
 def test_is_up_to_date(tmpdir: Path, mocker: MockerFixture) -> None:
     """spin.is_up_to_date compares mtimes of files correctly"""
-
     path1 = tmpdir / "{TEST_CUSTOM_FILE}"
     path2 = tmpdir / "foo"
     path3 = tmpdir / "baz"
@@ -577,10 +565,264 @@ def test_run_spin() -> None:
         assert repr(spin.cli.commands.call_args_list[0]) == "call(['1'])"
 
 
-# TODO: add more as soon as sources is clear
 def test_get_sources(cfg: ConfigTree) -> None:
+    """
+    spin.sources retrieves either the tree's 'sources' attribute values as
+    list or and an empty list
+    """
     assert spin.get_sources(cfg) == []
 
+    cfg["sources"] = "foo"
+    assert spin.get_sources(cfg) == ["foo"]
 
-###
-#
+    cfg["sources"] = ["foo", "bar"]
+    assert spin.get_sources(cfg) == ["foo", "bar"]
+
+
+def test_build_target(cfg: ConfigTree, mocker: MockerFixture) -> None:
+    """
+    spin.build_target extends the ConfigTree and is called using the expected
+    arguments
+    """
+    mocker.patch("subprocess.run")
+    cfg["build-rules"] = spin.config(
+        phony=spin.config(sources="notphony"),
+        notphony=spin.config(script=["1", "2"]),
+    )
+    # According to the rule above, building "phony" would require
+    # "notphony" to exist, which would be produced by calling 1 and 2.
+    spin.build_target(cfg, "phony", True)
+    assert [c.args[0][0] for c in subprocess.run.call_args_list] == ["1", "2"]  # type: ignore[attr-defined]
+
+
+def test_build_target_no_target(cfg: ConfigTree) -> None:
+    """spin.build_target fails if 'target' is not found"""
+    with pytest.raises(
+        click.Abort, match=".*Sorry, I don't know.* 'NOT_EXISTING_THING'.*"
+    ):
+        spin.build_target(spin.config(), target="NOT_EXISTING_THING")
+
+
+def test_build_target_no_target_but_exists(cfg: ConfigTree, tmpdir: Path) -> None:
+    """
+    spin.build_target will not fail but do nothing if the target not in the
+    tree's build rules, but still exist
+    """
+    cfg["TMPDIR"] = tmpdir
+    assert spin.build_target(spin.config(), target="{TMPDIR}") is None
+
+
+def test_build_target_up_to_date(
+    cfg: ConfigTree,
+    mocker: MockerFixture,
+    tmpdir: Path,
+) -> None:
+    """spin.build_target will not build anything if the target is up-to-date"""
+    mocker.patch("spin.info")
+    cfg["TMPDIR"] = tmpdir
+    cfg["build-rules"] = spin.config()
+    cfg["build-rules"]["{TMPDIR}"] = spin.config(script=["mkdir", "{TMPDIR}"])
+    spin.build_target(cfg, "{TMPDIR}", False)
+    assert "{TMPDIR} is up to date" in str(spin.info.call_args_list[1])  # type: ignore[attr-defined]
+
+
+def test_ensure(mocker: MockerFixture) -> None:
+    """spin.ensure is calling spin.build_target using the expected arguments"""
+
+    @spin.task()
+    def command() -> None:
+        """Just a command"""
+
+    mocker.patch("spin.build_target")
+    assert spin.ensure(command) is None
+    assert spin.build_target.call_args_list[0][0][-1] == "task command"
+    assert str(spin.build_target.call_args_list[0][1]) == "{'phony': True}"
+
+
+def test_argument() -> None:
+    """
+    spin.argument is returning a wrapper function which can be used to modify
+    commands
+    """
+    argument = spin.argument()
+    assert isinstance(argument, Callable)
+    assert isinstance(argument("name"), Callable)
+
+    argument = spin.argument(type=str, required=True)
+    task = spin.task("test")
+
+    def test_command() -> None:
+        pass
+
+    decorated_command = argument("param")(task(test_command))
+    assert decorated_command.params[0].name == "param"
+    assert isinstance(decorated_command.params[0].type, click.types.StringParamType)
+    assert decorated_command.params[0].required
+
+    with pytest.raises(TypeError, match=".* got an unexpected keyword argument 'help'"):
+        argument = spin.argument(help="Arguments must not implement 'help'")
+        argument("param")(task(test_command))
+
+
+def test_option() -> None:
+    """
+    spin.option is returning a wrapper function which can be used to modify
+    commands
+    """
+    task = spin.task("test")
+    option = spin.option("-p", "--param", type=str, required=True, help="Help me!")(
+        "param"
+    )
+    assert isinstance(option, Callable)
+
+    def test_command() -> None:
+        pass
+
+    decorated_command = option(task(test_command))
+    assert decorated_command.params[0].name == "param"
+    assert isinstance(decorated_command.params[0].type, click.types.StringParamType)
+    assert decorated_command.params[0].required
+    assert decorated_command.params[0].help == "Help me!"
+
+
+def test_task_regular() -> None:
+    """
+    spin.task is returning a wrapper function which will build and return a task
+    object with regular_callback #1
+    """
+    wrapper = spin.task()
+    assert isinstance(wrapper, Callable)
+
+    def test_command_regular(args: Any) -> Any:
+        """Function used for applying spin.task"""
+        return args
+
+    task = wrapper(test_command_regular)
+    assert isinstance(task, Callable)
+    assert task.name == "test-command-regular"
+    assert task.context_settings["ignore_unknown_options"]
+    assert task.context_settings["allow_extra_args"]
+    assert "regular_callback" in str(task.callback)
+    assert task.__doc__ == "Function used for applying spin.task"
+    assert task.callback("bar") == "bar"
+
+
+def test_task_ctx() -> None:
+    """
+    spin.task is returning a wrapper function which will build and return a task
+    object with regular_callback #2
+    """
+    wrapper = spin.task()
+
+    def test_command_ctx(ctx: dict, args: Any) -> dict:
+        return ctx.obj
+
+    task = wrapper(test_command_ctx)
+    assert task.name == "test-command-ctx"
+    assert "regular_callback" in str(task.callback)
+    with click.Context(click.Command(""), obj={"foo": "bar"}) as ctx:
+        assert task.callback(ctx) == {"foo": "bar"}
+
+
+def test_task_cfg(cfg: ConfigTree) -> None:
+    """
+    spin.task is returning a wrapper function which will build and return a task
+    object with alternate_callback
+    """
+    from spin import cli
+
+    wrapper = spin.task(noenv=True, when="dummy-hook")
+
+    def test_command_cfg(cfg: ConfigTree, args: Any) -> str:
+        return "foo"
+
+    task = wrapper(test_command_cfg)
+
+    assert task.name == "test-command-cfg"
+    assert isinstance(task.context_settings, ConfigTree)
+    assert "alternate_callback" in str(task.callback)
+    assert task.callback(cfg) == "foo"
+    assert "test-command-cfg" in cli.NOENV_COMMANDS
+    assert "dummy-hook" in cfg.hooks
+
+
+def test_group(cfg: ConfigTree) -> None:
+    """spin.group can be used to define command groups with subcommands"""
+    from spin import cli
+
+    @spin.group("foo", noenv=True)
+    def foo(ctx: click.Context) -> None:  # pylint: disable=disallowed-name
+        """Subcommands used for testing"""
+
+    assert isinstance(foo, spin.cli.GroupWithAliases)
+    assert "foo" in cli.NOENV_COMMANDS
+    assert foo.__doc__ == "Subcommands used for testing"
+
+    @foo.task("bar")
+    def bar() -> str:  # pylint: disable=disallowed-name
+        return "foo"
+
+    assert "bar" in foo.commands
+    assert foo.commands["bar"] is bar  # pylint: disable=comparison-with-callable
+    assert foo.commands["bar"].callback() == bar.callback() == "foo"
+
+    @foo.task("buz")
+    def buz(cfg: ConfigTree) -> ConfigTree:
+        return cfg
+
+    assert list(foo.commands.keys()) == ["bar", "buz"]
+
+
+def test_invoke(monkeypatch: pytest.MonkeyPatch) -> None:
+    """spin.invoke is able to invoke tasks marked to run 'when="lint"'"""
+    calls = []
+
+    def mock_print(*args: Any) -> None:
+        calls.append(args)
+
+    monkeypatch.setattr("builtins.print", mock_print)
+
+    @spin.task(when="lint")
+    def pylint() -> None:
+        """Should be invoked by 'lint'"""
+        print("pylint")
+
+    @spin.task(when="lint")
+    def flake8() -> None:
+        """Should be invoked by 'lint'"""
+        print("flake8")
+
+    def parse() -> None:  # pylint: disable=unused-variable
+        """Should not be invoked by 'lint'"""
+        print("parse")
+
+    with click.Context(click.Command("")):
+        spin.invoke("lint")
+    assert calls == [("pylint",), ("flake8",)]
+
+
+def test_parse_version() -> None:
+    """
+    spin.parse_version is parsing version strings correctly into
+    packaging.version.Version instances
+    """
+    from packaging.version import InvalidVersion, Version
+
+    version_str = "1.2.3"
+    parsed_version = spin.parse_version(version_str)
+
+    assert isinstance(parsed_version, Version)
+    assert str(parsed_version) == version_str
+
+    with pytest.raises(InvalidVersion):
+        spin.parse_version("invalid_version")
+
+
+def test_get_requires(cfg) -> None:
+    """
+    spin.requires is returning the expected 'requires' values for a +subtree
+    """
+    assert spin.get_requires(cfg, "build") == []
+    foo_requires = spin.config(build="foo")
+    cfg["requires"] = spin.config(foo=foo_requires)
+    assert spin.get_requires(cfg, "foo") is foo_requires
